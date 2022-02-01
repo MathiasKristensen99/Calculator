9021d04ab278f359dbe1b5397fea7c274935fd2e
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-02-01 14:55:31 +0100

    Initial commit

---
+++ app/src/main/java/com/easv/calculator/Calculate.kt
@@ -0,0 +1,20 @@
+package com.easv.calculator
+
+class Calculate {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun divide(a: Int, b: Int): Int {
+        return a / b
+    }
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+}
